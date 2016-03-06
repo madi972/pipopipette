3d52c2960c1104e2b4dfd60685412acdd542a2b2
lalolilolu <antoine.maslowski@gmail.com>
2016-03-06 19:18:01 +0100

    Update Graphe.java

--- src/poubelle/Graphe.java
+++ src/poubelle/Graphe.java
@@ -1,7 +1,8 @@
 package poubelle;
 import java.util.*;
 
-public class Graphe {
+public class Graphe 
+{
 	
 	private String graphe;
 	private int longueur;
@@ -12,70 +13,101 @@ public class Graphe {
 	 * 
 	 */
 	//1 contour,0 sans contour
-	public Graphe(int lon,int larg,int cont){
+	public Graphe(int lon,int larg,int cont)
+	{
 		
 		longueur = lon;
 		largeur = larg;
 		graphe = "";
 		if (cont == 1 ){
-			for(int i=0;i<2*lon-1;i++){
-				for(int j=0;j<2*larg-1;j++){
+			for(int i=0;i<2*lon-1;i++)
+			{
+				for(int j=0;j<2*larg-1;j++)
+				{
 					
 					//trait plein
-					if (i==0 || i==2*lon-2){ //première et dernière ligne
-						if (j%2==0){
+					if (i==0 || i==2*lon-2)
+					{ //première et dernière ligne
+						if (j%2==0)
+						{
 							graphe+=".";
-						}else{
+						}
+						else
+						{
 							graphe+="-";
 						}
 					}
 					//barres verticales et espaces
-					if(i%2!=0){		// pour chaque ligne impaire
-						if(j!=0 && j!=2*larg-2){
+					if(i%2!=0)// pour chaque ligne impaire
+					{		
+						if(j!=0 && j!=2*larg-2)
+						{
 							graphe+=" ";
-						}else{
+						}
+						else
+						{
 							graphe+="|";
 						}
 					}
 					
 					//ligne a remplir (en jouant)
-					if(i!=0 && i!=2*lon-2 && i%2==0){ // pour toute ligne qui n'est pas la première
-						if (j%2==0){
+					if(i!=0 && i!=2*lon-2 && i%2==0)// pour toute ligne qui n'est pas la première
+					{ 
+						if (j%2==0)
+						{
 							graphe+=".";
-						}else{
+						}
+						else
+						{
 							graphe+=" ";
 						}
 					}
 				}
 				graphe+="\n";
 			}
-		}else{
+		}
+		else
+		{
 			//graphe sans bords
-			for(int i=0;i<2*lon-1;i++){
-				for(int j=0;j<2*larg-1;j++){
+			for(int i=0;i<2*lon-1;i++)
+			{
+				for(int j=0;j<2*larg-1;j++)
+				{
 					
 					//trait plein
-					if (i==0 || i==2*lon-2){ //première et dernière ligne
-						if (j%2==0){
+					if (i==0 || i==2*lon-2)//première et dernière ligne
+					{ 
+						if (j%2==0)
+						{
 							graphe+=".";
-						}else{
+						}
+						else
+						{
 							graphe+=" ";
 						}
 					}
 					//barres verticales et espaces
-					if(i%2!=0){		// pour chaque ligne impaire
-						if(j!=0 || j!=2*larg-2){
+					if(i%2!=0)// pour chaque ligne impaire
+					{		
+						if(j!=0 || j!=2*larg-2)
+						{
 							graphe+=" ";
-						}else{
+						}
+						else
+						{
 							graphe+=" ";
 						}
 					}
 					
 					//ligne a remplir (en jouant)
-					if(i!=0 && i!=2*lon-2 && i%2==0){ // pour toute ligne qui n'est pas la première
-						if (j%2==0){
+					if(i!=0 && i!=2*lon-2 && i%2==0) // pour toute ligne qui n'est pas la première
+					{ 
+						if (j%2==0)
+						{
 							graphe+=".";
-						}else{
+						}
+						else
+						{
 							graphe+=" ";
 						}
 					}
@@ -85,12 +117,20 @@ public class Graphe {
 		}
 	}
 	
+	
+	/**
+	 * parcour le graphe
+	 * @param p1 un des 2 point jouer
+	 * @param p2 l'autre
+	 * @return la position entre les deux point
+	 */
 	public int parcour(int p1, int p2)
 	{
-		int compt1 = 0;
+		int compt1 = -1;
 		int compt2 = 0;
 		if(p1 - p2 == 1 || p1 - p2 == -1)
 		{
+			// si le coup est un _
 			while(compt1 != p1)
 			{
 				if(graphe.charAt(compt2) == '.')
@@ -99,10 +139,11 @@ public class Graphe {
 				}
 				compt2++;
 			}
-			compt2 += p1 - p2;
+			compt2 += p1 - p2+1;
 		}
 		else
 		{
+			// si le coup est un |
 			while(compt1 != p1)
 			{
 				if(graphe.charAt(compt2) == '.')
@@ -113,16 +154,22 @@ public class Graphe {
 			}
 			if (p1 < p2)
 			{
-				compt2 += largeur;
+				compt2 += largeur+largeur-1;
 			}
 			else
 			{
-				compt2 -= largeur;
+				compt2 -= largeur+largeur-1;
 			}
 		}
 		return compt2;
 	}
 	
+	/**
+	 * verifie si le coup est possible
+	 * @param p1 un des 2 point jouer
+	 * @param p2 l'autre
+	 * @return si le coup est possible
+	 */
 	public boolean coupPossible(int p1, int p2)
 	{
 		boolean res = true;
@@ -141,11 +188,20 @@ public class Graphe {
 		return res;
 	}
 	
+	/**
+	 * change un charactere lorsque l'on joue
+	 * @param p1 un des 2 point jouer
+	 * @param p2 l'autre
+	 * @param tour tour du joueur qui agit
+	 * @return le tour du joueur si il a changer ou non (marquer un point ou non)
+	 */
 	public int coup (int p1, int p2, int tour)
 	{
 		int res = tour;
-		char c = 'a';
+		char c = ' ';
 		int pos = this.parcour(p1, p2);
+		System.out.println(pos);
+		// choix du charactere a mettre
 		if (p1 - p2 == -1 || p1 - p2 == 1)
 		{
 			c = '-';
@@ -154,14 +210,14 @@ public class Graphe {
 		{
 			c ='|';
 		}
-		graphe = graphe.substring(0,pos) + c +graphe.substring(pos+1);
+		graphe = graphe.substring(0,pos) + c +graphe.substring(pos+1); // insere le charactere a la place voulu
 		return res;
 	}
 	
-	public static void main (String [] args)
-	{
-		Graphe a = new Graphe(5,5,1);
+	public static void main (String [] args){
+		Graphe a = new Graphe(4,4,1);
 		System.out.print(a.graphe);
-		
+		a.coup(1, 5, 1);
+		System.out.print(a.graphe);
 	}
 }
